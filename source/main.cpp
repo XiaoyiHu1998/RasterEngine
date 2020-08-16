d15f753db6f8111e482af9d38acd57b88dbf784a
Xiao yi Hu <xy1998@live.nl>
2020-08-16 18:32:41 +0200

    added glew library and now draws a triangle on to the screen

--- source/main.cpp
+++ source/main.cpp
@@ -1,26 +1,41 @@
+#define GLEW_STATIC
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
 #include <iostream>
-// #include "..\dependencies\include\GLFW\glfw3.h"
-#include "glfw3.h"
 
 int main(){
-    GLFWwindow* windowPointer;
+    GLFWwindow* window;
 
     if(!glfwInit()){
         return -1;
     }
 
-    windowPointer = glfwCreateWindow(640, 480, "RasterEngine.exe", NULL, NULL);
-    if(!windowPointer){
+    window = glfwCreateWindow(640, 480, "RasterEngine.exe", NULL, NULL);
+    if(!window){
         glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent(windowPointer);
-    while(!glfwWindowShouldClose(windowPointer)){
+    glfwMakeContextCurrent(window);
+    // if(glewInit() != GLEW_OK){
+    //     return -1;
+    // }
+    
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    while(!glfwWindowShouldClose(window)){
         //rendering
         glClear(GL_COLOR_BUFFER_BIT);
+    
+        glBegin(GL_TRIANGLES);
+        glVertex2f(1.0f, -1.0f);
+        glVertex2f(-1.0f, 1.0f);
+        glVertex2f(1.0f, 1.0f);
+        glEnd();
+
+
         //swap buffers
-        glfwSwapBuffers(windowPointer);
+        glfwSwapBuffers(window);
+
         //event check and handling
         glfwPollEvents();
     }
