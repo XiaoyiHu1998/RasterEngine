6297a0fff1875fe3f64d63bebb6f156086cfaf2d
Xiao yi Hu <xy1998@live.nl>
2020-08-17 19:14:36 +0200

    added imgui, is now compilable, but doesnt show the imgui window

--- source/main.cpp
+++ source/main.cpp
@@ -1,38 +1,59 @@
 #define GLEW_STATIC
-#include "GL\glew.h"
-#include "GLFW\glfw3.h"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "IMGUI/imgui.h"
+#include "IMGUI/imgui_impl_glfw.h"
+#include "IMGUI/imgui_impl_opengl3.h"
 #include "masterInclude.hpp"
 
 
 int main(){
-    GLFWwindow* window;
-
+    //glfw init
     if(!glfwInit()){
         return -1;
     }
-
-    window = glfwCreateWindow(640, 480, "RasterEngine.exe", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "RasterEngine.exe", NULL, NULL);
     if(!window){
         glfwTerminate();
         return -1;
     }
-    
     glfwMakeContextCurrent(window);
+
+    //glew init
     if(glewInit() != GLEW_OK){
         return -1;
     }
+
+    //imgui init
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+    bool show_demo_window = true;
     
+    //main loop
     std::cout << glGetString(GL_VERSION) << std::endl;
     while(!glfwWindowShouldClose(window)){
-        //rendering
+        //imgui frame
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init((char *)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+        //OpenGL rendering
         glClear(GL_COLOR_BUFFER_BIT);
-    
         glBegin(GL_TRIANGLES);
         glVertex2f(1.0f, -1.0f);
         glVertex2f(-1.0f, 1.0f);
         glVertex2f(1.0f, 1.0f);
         glEnd();
-
 
         //swap buffers
         glfwSwapBuffers(window);
@@ -40,4 +61,12 @@ int main(){
         //event check and handling
         glfwPollEvents();
     }
+
+    //Terminate
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
