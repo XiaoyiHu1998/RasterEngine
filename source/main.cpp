9178d12d5b809ffc909bc3f44823ca6fae1ccc90
Xiao yi Hu <xy1998@live.nl>
2020-08-17 23:32:46 +0200

    edited the ImGui frame to only display framerate and frametime for now

--- source/main.cpp
+++ source/main.cpp
@@ -38,51 +38,38 @@ int main(){
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
     bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     
     //main loop
     std::cout << glGetString(GL_VERSION) << std::endl;
     while(!glfwWindowShouldClose(window)){
-        //imgui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-        ImGui::ShowDemoWindow(&show_demo_window);
-        static float f = 0.0f;
-        static int counter = 0;
 
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox("Another Window", &show_another_window);
-
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        ImGui::End();
-
-        //OpenGL rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
+        //clear framebuffer
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //openGL rendering
         glBegin(GL_TRIANGLES);
         glVertex2f(1.0f, -1.0f);
         glVertex2f(-1.0f, 1.0f);
         glVertex2f(1.0f, 1.0f);
         glEnd();
 
+        //imgui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::Begin("System Info");
+        ImGui::Text("(%.1f FPS)", ImGui::GetIO().Framerate);
+        ImGui::Text("FrameTime: %.3f ms", 1000.0f / ImGui::GetIO().Framerate);
+        ImGui::End();
+
+        //render imgui overlay
+        ImGui::Render();
+        int display_w, display_h;
+        glViewport(0, 0, display_w, display_h);
+        glfwGetFramebufferSize(window, &display_w, &display_h);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         //swap buffers
--- source/masterInclude.hpp
+++ source/masterInclude.hpp
@@ -6,8 +6,8 @@
 #include <vector>
 #include <string>
 
-#define WINDOW_RES_X 1366
-#define WINDOW_RES_Y 768
+#define WINDOW_RES_X 960
+#define WINDOW_RES_Y 540
 #define WINDOW_PIXEL_COUNT WINDOW_RES_X * WINDOW_RES_Y
 
 #define THREAD_COUNT std::thread::hardware_concurrency();
