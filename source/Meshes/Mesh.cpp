b725677c0656d53e41d718cbcc8ce329cdbd32ca
Xiao yi Hu <xy1998@live.nl>
2020-09-12 00:55:43 +0200

    added prelimenary code for vbo triangle rendering to Mesh for test purposes

--- source/Meshes/Mesh.cpp
+++ source/Meshes/Mesh.cpp
@@ -7,9 +7,39 @@ Mesh::Mesh(){
 }
 
 void Mesh::render(){
-    glBegin(GL_TRIANGLES);
-    glVertex2f(0.0f, 0.7f);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
+    // glBegin(GL_TRIANGLES);
+    // glVertex2f(0.0f, 0.7f);
+    // glVertex2f(-0.5f, -0.5f);
+    // glVertex2f(0.5f, -0.5f);
+    // glEnd();
+
+    float vertices[9] = {
+         0.0f,   0.7f,  0.0f,
+        -0.5f,  -0.5f,  0.0f,
+         0.5f,  -0.5f,  0.0f
+    };
+    std::cout << "vertices" << std::endl;
+
+    unsigned int vertexBuffer;
+    glCreateBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_BUFFER, vertexBuffer);
+    std::cout << "create and bind buffer" << std::endl;
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    std::cout << "set bufferdata" << std::endl;
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    std::cout << "enabled vertex arrays" << std::endl;
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+    std::cout << "set vertexPointer" << std::endl;
+
+    std::cout << "start drawing arrays" << std::endl;
+    glDrawArrays(GL_TRIANGLES, 0, 9);
+    std::cout << "drawn arrays" << std::endl;
+
+    glDeleteBuffers(1, &vertexBuffer);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    std::cout << "deleted buffers" << std::endl;
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
