55a2c562455a3a885fa0f8ffd6aacd7fece4d521
Xiao yi Hu <xy1998@live.nl>
2020-09-12 00:54:01 +0200

    scenes now add a mesh to the vector in the initializer for testing purposes, render calls render on all meshes

--- source/Scene/Scene.cpp
+++ source/Scene/Scene.cpp
@@ -1,7 +1,12 @@
 #include "Scene.hpp"
 
-Scene::Scene(){}
+Scene::Scene(){
+    meshVector.push_back(Mesh());
+}
 
 void Scene::render(){
-
+    for(int i = 0; i < meshVector.size(); i++){
+        std::cout << meshVector.size() << std::endl;
+        meshVector[i].render();
+    }
 }
