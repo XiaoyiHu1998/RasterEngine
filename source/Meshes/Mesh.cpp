192716f4a82d2c8a5822ad12a10bab2f8a0606ab
Xiao yi Hu <xy1998@live.nl>
2020-09-24 22:57:17 +0200

    Mesh constructor now actually assigns the buffers and shader in parameter to its member variables. added debugging cout statements in render(). changed opengl code in render() for drawing Meshes with their own vbo's and ibo's.

--- source/Meshes/Mesh.cpp
+++ source/Meshes/Mesh.cpp
@@ -1,36 +1,47 @@
 #include "Mesh.hpp"
 
-Mesh::Mesh(VertexBuffer vertexBuffer, IndexBuffer indexBuffer, Shader shader){
-    vertexPositionDimensions = 3;
-    vertexNormalComponentCount = 3;
-    vertexTextureCoordinateCount = 2;
-}
+Mesh::Mesh(const VertexBuffer vertexBuffer, const IndexBuffer indexBuffer, const Shader shader):
+    vertexBuffer{vertexBuffer},
+    indexBuffer{indexBuffer},
+    shader{shader}
+    {
+        vPosDimensions = 3;
+        vNormalDimensions = 3;
+        vTexDimensions = 2;
+    }
 
 void Mesh::render(){
-    // indexBuffer.bind();
-    // vertexBuffer.bind();
-    // GLCall(glEnableVertexAttribArray(0));
+    std::cout << "binding buffers" << std::endl;
+    vertexBuffer.bind();
+    vertexBuffer.bind();
+
+    std::cout << "setting clientstate" << std::endl;
+    GLCall(glEnableClientState(GL_VERTEX_ARRAY));
+    std::cout << "setting vertexPointer" << std::endl;
+    GLCall(glVertexPointer(3, GL_FLOAT, 0, 0));
+
+    std::cout << "drawing buffers, " << "indexBufferCount: " << indexBuffer.getCount() << std::endl;
+    GLCall(glDrawElements(GL_TRIANGLES, indexBuffer.getCount(), GL_UNSIGNED_INT, 0));
+    std::cout << "disabling client state" << std::endl;
+    GLCall(glDisableClientState(GL_VERTEX_ARRAY));
+
+    // float vertices[] = {
+    //      0.0f,   0.7f,  0.0f,
+    //     -0.5f,  -0.5f,  0.0f,
+    //      0.5f,  -0.5f,  0.0f
+    // };
+
+    // unsigned int vertexBuffer;
+    // glCreateBuffers(1, &vertexBuffer);
+    // glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // glEnableVertexAttribArray(0);
+
     // GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0));
-    // GLCall(glDrawArrays(GL_TRIANGLES, 0, vertexBuffer.getSize() / sizeof(float)));
-    // GLCall(glDisableVertexAttribArray(0));
 
-    float vertices[] = {
-         0.0f,   0.7f,  0.0f,
-        -0.5f,  -0.5f,  0.0f,
-         0.5f,  -0.5f,  0.0f
-    };
+    // glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(float));
 
-    unsigned int vertexBuffer;
-    glCreateBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-    glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(float));
-
-    glDeleteBuffers(1, &vertexBuffer);
-    glDisableVertexAttribArray(0);
+    // glDeleteBuffers(1, &vertexBuffer);
+    // glDisableVertexAttribArray(0);
 }
