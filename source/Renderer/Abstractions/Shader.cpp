761b7dc04e027d48b892d96cc179f6cc5647a3ed
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2021-01-09 19:34:39 +0100

    added code to read from files and create a shader program in Shader class, compile error related to GLCall

--- source/Renderer/Abstractions/Shader.cpp
+++ source/Renderer/Abstractions/Shader.cpp
@@ -1,13 +1,94 @@
 #include "Shader.hpp"
 
 Shader::Shader(const char* fragmentShader, const char* vertexShader){
+    //get shadercode in const char* format
+    std::string fragShader;
+    std::string vertShader;
+    std::ifstream file;
+    // std::cout << "fragmentShader: " << fragmentShader << std::endl;
+    // std::cout << "vertexShader: " << vertexShader << std::endl;
+
+    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try{
+        std::stringstream fragShaderStream;
+        std::stringstream vertShaderStream;
+
+        file.open(fragmentShader);
+        fragShaderStream << file.rdbuf();
+        fragShader = fragShaderStream.str();
+        file.close();
+        
+        file.open(vertexShader);
+        vertShaderStream << file.rdbuf();
+        file.close();
+        vertShader = vertShaderStream.str();
+    }
+    catch(std::ifstream::failure error){
+        std::cout << "ERROR: Failed to load shader files." << std::endl;
+    }
+    // std::cout << "fragmentShaderContent: " << fragShader << std::endl;
+    // std::cout << "vertexShaderContent: " << vertShader << std::endl;
+    const char* fragShaderCode = fragShader.c_str();
+    const char* vertShaderCode = vertShader.c_str();
+    
+    //create shaders
+    int shaderSuccess;
+    char shaderLog[512];
+
+    //fragmentShader
+    unsigned int fragShaderID = GLCall(glCreateShader(GL_FRAGMENT_SHADER));
+    GLCall(glShaderSource(fragShaderID, &fragShaderCode, NULL));
+    GLCall(glShaderCompile(fragShaderID));
+    GLCall(glGetShaderiv(fragShaderID, GL_COMPILE_STATUS, &shaderSuccess));
+    if(!shaderSuccess){
+        GLCall(glGetShaderInfoLog(fragShaderID, 512, NULL, shaderLog));
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILE_ERROR" << shaderLog << std::endl;
+    }
+    
+    //vertexShader
+    unsigned int vertShaderID = GLCall(glCreateSHader(GL_VERTEX_SHADER));
+    GLCall(glShaderSource(vertShaderID, &vertShaderCode, NULL));
+    GLCall(glShaderCompile(vertShaderID));
+    GLCall(glGetShaderiv(vertShaderID, GL_COMPILE_STATUS, &shaderSuccess));
+    if(!shaderSuccesss){
+        GLCall(glGetShaderInfoLog(vertShaderID, 512, NULL, shaderLog));
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILE_ERROR" << shaderLog << std::endl;
+    }
+
+    //shaderProgram
+    unsigned int shaderProgramID = GLCall(glCreateProgram());
+    GLCall(glAttachShader(shaderProgramID, fragShaderID));
+    GLCall(glAttachShader(shaderProgramID, vertShaderID));
+    GLCall(glLinkProgram(shaderProgramID));
+    GLCall(glGetShaderiv(shaderProgramID, GL_LINK_STATUS, &shaderSuccess));
+    if(!shaderSuccess){
+        GLCall(glGetShaderInfoLog(shaderProgramID, 512, NULL, shaderLog));
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILE_ERROR" << shaderLog << std::endl;
+    }
+    
+    GLCall(glDeleteShader(fragShaderID));
+    GLCall(glDeleteShader(vertShaderID));
+    
+    shaderID = shaderProgramID;
     //glCreateProgram(&programID);
 }
 
+void Shader::setBool(const std::string& uniformName, bool value){
+    GLCall(glUniform1i(glGetUniformLocation(shaderID, uniformName.c_str()), std::static_cast<int>(value)));
+}
+
+void Shader::setInt(const std::string& uniformName, int value){
+    GLCall(glUniform1i(glGetUniformLocation(shaderID, uniformName.c_str()), value));
+}
+
+void Shader::setFloat(const std::string& uniformName, float value){
+    GLCall(glUniform1f(glGetUniformLocation(shaderID, uniformName.c_str()), value));
+}
+
 Shader::~Shader(){
-    //glDeleteProgram(&programID);
+    GLCall(glDeleteProgram(shaderID));
 }
 
 void Shader::useProgram(){
-    // glUseProgram(programID);
+    GLCall(glUseProgram(shaderID));
 }
--- source/Renderer/Abstractions/Shader.hpp
+++ source/Renderer/Abstractions/Shader.hpp
@@ -1,12 +1,21 @@
 #ifndef SHADER_HPP
 #define SHADER_HPP
 
+#include "masterinclude.hpp"
+#include <fstream>
+#include <sstream>
+
 class Shader{
 private:
+    uint32_t shaderID;
 public:
-    Shader(const char* fragmentShader = "../../assets/Shaders/defaultFragmentShader.txt", const char* vertexShader = "../../assets/Shaders/defaultVertexShader.txt");
+    Shader(const char* fragmentShader = "D:\\Git_repos\\RasterEngine\\source\\Renderer\\Abstractions\\defaultFragmentShader.txt", const char* vertexShader = "D:\\Git_repos\\RasterEngine\\source\\Renderer\\Abstractions\\defaultVertexShader.txt");
+    // Shader(const char* fragmentShader = "../../assets/Shaders/defaultFragmentShader.txt", const char* vertexShader = "../../assets/Shaders/defaultVertexShader.txt");
     ~Shader();
     void useProgram();
+    void setBool(const std::string& uniformName, bool value);
+    void setInt(const std::string& uniformName, int value);
+    void setFloat(const std::string& uniformName, float value);
     void bindUniforms();
 };
 
